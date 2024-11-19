c36da2bd92b403fd804dc3d2cc3512922cb7ba00
Kelvin Ekisa <kelvinekisa8@gmail.com>
2024-11-19 15:16:54 +0300

    layout

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,24 +1,11 @@
 package com.example.composetutorial
 
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,17 +13,26 @@ class MainActivity : ComponentActivity() {
         setContent { // defines the activities layout where composable functions are called.
             Text("Hello world!")
 //            MessageCard("Kelvin EKisa")
+            MessageCard(Message("Kelvin", "Android Jetpack"))
         }
     }
 }
 // Define a composable function
 
 
+// layout
+data class Message(val author: String, val body: String)
+
+//@Composable
+//fun MessageCard(name: String) {
+//    Text(text = "Hello $name!",
+//        color = Color.Red
+//    )
+//}
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!",
-        color = Color.Red
-    )
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 // preview your composable annotation
@@ -44,5 +40,7 @@ fun MessageCard(name: String) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Kelvin Ekisa")
+    MessageCard(
+        msg = Message("Kelvin", "Hey, take a look at jetpack compose")
+    )
 }
